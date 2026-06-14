1ad7dd7729de7a382731d5afa6e6e61c5bb3a439
Abanoub Nashat <nashatabanop@gmail.com>
2026-06-14 14:56:58 +0300

    feat: implement item removal and clear all items functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-input');
 const itemsList = document.querySelector('#item-list');
+const clearBtn = document.querySelector('#clear');
 
 
 // create the list item
@@ -47,6 +48,35 @@ const addItem = (e) => {
   itemInput.focus();
 }
 
+// remove one item.
+const removeItem = (e) => {
+  // this what chat told me to follow as a common pattern as it will always targets the button right
+  const button = e.target.closest('.remove-item');
 
-// even listener on form submit;
+  if (button) {
+    button.parentElement.remove();
+  }
+
+  // this solution of we added a span as a parent for the i element here the code breaks.
+  // if (e.target.parentElement.classList.contains('remove-item')) {
+  //   e.target.parentElement.parentElement.remove();
+  // }
+}
+
+// remove all items
+const removeItems = (e) => {
+  // this what chatgpt asked me to do as it's more modern and less code to have
+  itemsList.replaceChildren();
+
+  //perfectly fine to use it but more lines to do
+  // while (itemsList.firstChild) {
+  //   itemsList.firstChild.remove();
+  // }
+}
+
+// event listener on form submit;
 itemForm.addEventListener('submit', addItem);
+// event listener to delete an item using event delegation.
+itemsList.addEventListener('click', removeItem);
+// event listener to clear all items
+clearBtn.addEventListener('click', removeItems);
